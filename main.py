78accee146b2ce2f1d511861f06eee6d8c7dea68
Sebasxia123 <mendezbarriossebastian@gmail.com>
2025-02-03 17:40:56 -0600

    Update main.py
    hola

--- main.py
+++ main.py
@@ -1,44 +1,24 @@
 import discord
-from bot_logic import gen_pass
-# La variable intents almacena los privilegios del bot
+from discord.ext import commands
+
 intents = discord.Intents.default()
-# Activar el privilegio de lectura de mensajes
 intents.message_content = True
-# Crear un bot en la variable cliente y transferirle los privilegios
-client = discord.Client(intents=intents)
 
-@client.event
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
 async def on_ready():
-    print(f'Hemos iniciado sesión como {client.user}')
+    print(f'We have logged in as {bot.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith('$hello'):
-        await message.channel.send("Hi!")
-    elif message.content.startswith('$bye'):
-        await message.channel.send("\U0001f642")
-    elif message.content.startswith('$saluda'):
-        await message.channel.send("Hola")
-    elif message.content.startswith('$bot_hacme_un_pastel'):
-        await message.channel.send(gen_pass(10))
-    elif message.content.startswith('$que es este server'):
-        await message.channel.send('es para estar juntos (:')
-    elif message.content.startswith('$quien_creo_este_server'):
-        await message.channel.send("el_rey_potato.")
-    elif message.content.startswith('$me estan asiendo buling'):
-        await message.channel.send("gracias por informarnos te ayudaremos cuando posible (:")
-    elif message.content.startswith('$puedo jugar minecraft?'):
-        await message.channel.send("si!,si puedes jugarlo") 
-    elif message.content.startswith('$hola podemos hablar'):
-        await message.channel.send("si!(:")
-    elif message.content.startswith('$manda un emoji'):
-        await message.channel.send("\U0001f642")              
-        
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hola, soy un bot {bot.user}!')
 
-                
-    else:
-        await message.channel.send("que dijiste?")
-
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right) 
 client.run("")
